bb8f04f9613fa0d33df5e77317577fca9e7412e6
plittlewood-rpt <plittlewood@reapit.com>
2018-07-13 09:27:07 +0100

    Added a picturwe

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 echo "Welcome to my really basic website";
 
 echo "<a href='http://www.google.co.uk'>Here is a link to google. Click it!</a>";
+echo "<hr>"
+echo "<p>Here is a funny picture</p>";
+echo "<img src='https://www.askideas.com/media/47/Eating-Watermelon-Like-A-Boss-Funny-Cool-Meme-Image.jpg' />";
+
 ?>
