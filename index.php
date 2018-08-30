aa007a4293f6f844f854ad2cf6ab508847929838
Pete Littlewood <plittlewood@reapit.com>
2018-08-30 14:47:48 +0100

    Changed picture and caption

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo "Welcome to my really, really basic website which is used to play with Bamb
 
 echo "<a href='http://www.google.co.uk' target='_blank'>Here is a link to Google. Click it!</a>";
 echo "<hr>";
-echo "<p>Here is a funny picture</p>";
-echo "<img src='https://www.askideas.com/media/47/Eating-Watermelon-Like-A-Boss-Funny-Cool-Meme-Image.jpg' />";
+echo "<p>Here is a different funny picture</p>";
+echo "<img src='http://www.dumpaday.com/wp-content/uploads/2016/10/funny-babies.jpg' />";
 
 ?>
