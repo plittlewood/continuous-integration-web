a6ec0d519bfdb9613dadf7f6c830b1f233de6b0c
Pete Littlewood <plittlewood@reapit.com>
2018-08-30 09:42:25 +0100

    Google link now opens in new tab. Updated header text

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-echo "Welcome to my really basic website";
+echo "Welcome to my really, really basic website which is used to play with Bamboo";
 
-echo "<a href='http://www.google.co.uk'>Here is a link to google. Click it!</a>";
+echo "<a href='http://www.google.co.uk' target='_blank'>Here is a link to Google. Click it!</a>";
 echo "<hr>";
 echo "<p>Here is a funny picture</p>";
 echo "<img src='https://www.askideas.com/media/47/Eating-Watermelon-Like-A-Boss-Funny-Cool-Meme-Image.jpg' />";
